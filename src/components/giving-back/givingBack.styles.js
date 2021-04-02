fd49b0a57f560a6ba10366377630b27f63bbf2b1
joe <jsaperstein@tangogroup.com>
2021-04-01 21:51:00 -0600

    border animation

--- src/components/giving-back/givingBack.styles.js
+++ src/components/giving-back/givingBack.styles.js
@@ -2,12 +2,89 @@ import { css } from '@emotion/react';
 import { pr4, pl4 } from '../layout/margin-padding-utils.styles';
 import { initiallyHidden } from '../shared/shared.styles';
 
-const styles = (theme) => css`
-  padding: 3rem;
+export const baseStyles = (theme) => css`
+  position: relative;
+
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+
   ${theme.breakpoints.up('md')} {
     padding: 6rem;
   }
+`;
 
+export const drawStyles = css`
+  transition: all 0.5s ease-in-out;
+  &::before,
+  &::after {
+    border: 3px solid transparent;
+    width: 0;
+    height: 0;
+  }
+
+  &::before {
+    top: 0;
+    left: 0;
+  }
+
+  &::after {
+    bottom: 0;
+    right: 0;
+  }
+
+  &::after {
+    top: 0;
+    left: 0;
+  }
+`;
+
+const styles = (theme) => css`
+  .draw {
+    &::before,
+    &::after {
+      width: 100%;
+      height: 100%;
+    }
+
+    &::before {
+      border-top-color: ${theme.palette.text.primary}; // Make borders visible
+      border-right-color: ${theme.palette.text.primary};
+      border-radius: 20px;
+      transition: width 0.5s ease-out,
+        // Width expands first
+        height 0.5s ease-out 0.5s; // And then height
+    }
+
+    &::after {
+      border-bottom-color: ${theme.palette.text
+        .primary}; // Make borders visible
+      border-left-color: ${theme.palette.text.primary};
+      border-radius: 20px;
+      transition: border-color 0s ease-out 0.5s,
+        // Wait for ::before to finish before showing border
+        width 0.5s ease-out 0.5s,
+        // And then expanding width
+        height 0.5s ease-out 0.75s; // And finally height
+    }
+
+    // Change colors
+    &::before {
+      border-top-color: ${theme.palette.text.primary};
+      border-right-color: ${theme.palette.text.primary};
+    }
+
+    &::after {
+      border-bottom-color: ${theme.palette.text.primary};
+      border-left-color: ${theme.palette.text.primary};
+      transition: // Animate height first, then width
+        height 0.5s ease-out, width 0.5s ease-out 0.5s;
+    }
+  }
   .giving-back-section {
     ${initiallyHidden}
     text-align: center;
--- src/components/giving-back/index.js
+++ src/components/giving-back/index.js
@@ -1,63 +1,80 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
 import { Container, Typography, Grid, Box } from '@material-ui/core';
-import styles from './givingBack.styles';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import styles, { baseStyles, drawStyles } from './givingBack.styles';
 import useScrollAnimation from '../../hooks/use-scroll-animation';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const GivingBackSection = ({ givingBackSection }) => {
   const theme = useTheme();
   const hideSm = useMediaQuery(theme.breakpoints.up('sm'));
   const leftSide = useRef(null);
   const rightSide = useRef(null);
+  const drawRef = useRef(null);
 
   useScrollAnimation(leftSide, [leftSide, rightSide]);
+
+  useEffect(() => {
+    gsap.from(drawRef.current, {
+      scrollTrigger: {
+        trigger: drawRef.current,
+        start: 'top +=150 center',
+        toggleClass: 'draw',
+      },
+    });
+  }, []);
 
   return (
     <Container maxWidth={false} className="section-padding" id="section4">
       <Container maxWidth="lg" className="side-padding">
-        <Grid container css={styles} className="border-container">
-          <Grid
-            item
-            xs={12}
-            md={6}
-            className="giving-back-section"
-            ref={leftSide}
-          >
-            <Typography variant="h5" component="h2">
-              {givingBackSection.leftSideTitle}
-            </Typography>
-            <Typography variant="body1">
-              {givingBackSection.leftSideCopy.leftSideCopy}
-            </Typography>
-            <Typography variant="body1">
-              {givingBackSection.orgsModalLinkText}
-            </Typography>
-          </Grid>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            className="pushing-diversity"
-            ref={rightSide}
-          >
-            <Box className="badges-container">
-              {givingBackSection.badges.map((badge) => (
-                <img key={badge.id} src={badge.file.url} />
-              ))}
-            </Box>
-            <Box className="diversity-header-section">
-              <Typography variant="h5" component="h2" className="title-right">
-                {hideSm && <img src={`images/arrow.svg`} />}
-                {givingBackSection.rightSideTitle}
+        <div css={styles}>
+          <Grid container ref={drawRef} css={[baseStyles, drawStyles]}>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              className="giving-back-section"
+              ref={leftSide}
+            >
+              <Typography variant="h5" component="h2">
+                {givingBackSection.leftSideTitle}
               </Typography>
-            </Box>
-            {/* <div ref={wrapper}></div> */}
-            <Typography variant="body1" className="copy-right">
-              {givingBackSection.rightSideCopy.rightSideCopy}
-            </Typography>
+              <Typography variant="body1">
+                {givingBackSection.leftSideCopy.leftSideCopy}
+              </Typography>
+              <Typography variant="body1">
+                {givingBackSection.orgsModalLinkText}
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              className="pushing-diversity"
+              ref={rightSide}
+            >
+              <Box className="badges-container">
+                {givingBackSection.badges.map((badge) => (
+                  <img key={badge.id} src={badge.file.url} />
+                ))}
+              </Box>
+              <Box className="diversity-header-section">
+                <Typography variant="h5" component="h2" className="title-right">
+                  {hideSm && <img src={`images/arrow.svg`} />}
+                  {givingBackSection.rightSideTitle}
+                </Typography>
+              </Box>
+              {/* <div ref={wrapper}></div> */}
+              <Typography variant="body1" className="copy-right">
+                {givingBackSection.rightSideCopy.rightSideCopy}
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
+        </div>
       </Container>
     </Container>
   );
